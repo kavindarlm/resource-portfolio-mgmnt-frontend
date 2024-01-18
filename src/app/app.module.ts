addfa8632862dc9f0eab61021752ecb9fe3af7cf
malintha <malinthakavinda24@gmail.com>
2024-01-19 02:30:14 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PagesBodyComponent } from './pages-body/pages-body.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PagesBodyComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
