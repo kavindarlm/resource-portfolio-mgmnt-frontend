b181fd685351a3011b813943393109a29cdf5f72
malintha <malinthakavinda24@gmail.com>
2024-01-19 15:56:41 +0530

    delete form example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesBodyComponent } from './pages-body/pages-body.component';
-import { FormComponent } from './form/form.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     PagesBodyComponent,
-    FormComponent,
   ],
   imports: [
     BrowserModule,
