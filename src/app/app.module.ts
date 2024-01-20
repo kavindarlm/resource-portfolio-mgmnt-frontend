c1d3b9f180dd79146d3fafdc487ef09a743bf359
215131E <112235122+215131E@users.noreply.github.com>
2024-01-20 22:35:43 +0530

    ASH 20/01/24 22:35PM
    Forget password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesBodyComponent } from './pages-body/pages-body.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PagesBodyComponent,
+    ForgotPasswordComponent,
   ],
   imports: [
     BrowserModule,
