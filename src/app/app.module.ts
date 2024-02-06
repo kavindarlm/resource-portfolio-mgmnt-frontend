bb97b9b1c47f8e33468faee4adc9949d8218f4f3
malintha <malinthakavinda24@gmail.com>
2024-02-07 01:48:03 +0530

    final frontend for user authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PagesBodyComponent } from './pages-body/pages-body.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { FgPsswdComponent } from './fg-psswd/fg-psswd.component';
-import { LoginComponent } from './login/login.component';
 import { LoginAccComponent } from './login-acc/login-acc.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagesBodyComponent,
-    ForgotPasswordComponent,
     FgPsswdComponent,
-    LoginComponent,
     LoginAccComponent,
   ],
   imports: [
