4c671a9a46c7e674df18142f524735f1ccb35fca
Savindi <savindipaboda@gmail.com>
2024-03-05 20:22:02 +0530

    orgUnitManagement-v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesBodyComponent } from './pages-body/pages-body.component';
+import { UnitTreeComponent } from './orgUnitMgt/unit-tree/unit-tree.component';
+import { UnitListComponent } from './orgUnitMgt/unit-list/unit-list.component';
+import { UnitFormComponent } from './orgUnitMgt/unit-form/unit-form.component';
+import { ReuseButtonComponent } from './orgUnitMgt/reuse-button/reuse-button.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PagesBodyComponent,
+    UnitTreeComponent,
+    UnitListComponent,
+    UnitFormComponent,
+    ReuseButtonComponent,
 
   ],
   imports: [
