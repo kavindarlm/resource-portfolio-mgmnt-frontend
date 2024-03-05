fa70c8da7664e20a323b5be2551835d5dc6935d6
Savindi <savindipaboda@gmail.com>
2024-03-05 20:41:26 +0530

    resourceManagement-v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesBodyComponent } from './pages-body/pages-body.component';
+import { AddFormComponent } from './resourceMgt/add-form/add-form.component';
+import { ButtonComponent } from './resourceMgt/button/button.component';
+import { FirstViewComponent } from './resourceMgt/first-view/first-view.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PagesBodyComponent,
+    AddFormComponent,
+    ButtonComponent,
+    FirstViewComponent,
 
   ],
   imports: [
