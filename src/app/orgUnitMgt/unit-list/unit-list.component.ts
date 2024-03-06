04e5e2084ef52e467bd06a8e554f9d686ca0da34
Savindi <savindipaboda@gmail.com>
2024-03-06 06:55:23 +0530

    orgUnitManagement-v2

--- src/app/orgUnitMgt/unit-list/unit-list.component.ts
+++ src/app/orgUnitMgt/unit-list/unit-list.component.ts
@@ -10,10 +10,77 @@ export class UnitListComponent implements OnInit{
   constructor() {
     this.showForm=false;
   }
+
   ngOnInit(): void {
 
   }
+
   showcomponent(): void{
     this.showForm =! this.showForm;
   }
+
+  // array: any = [
+  //   {
+  //     ID: "1",
+  //     Name: "Unit 1",
+  //     Parent: "",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "2",
+  //     Name: "Unit 2",
+  //     Parent: "Unit 1",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "3",
+  //     Name: "Unit 3",
+  //     Parent: "Unit 1",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "4",
+  //     Name: "Unit 4",
+  //     Parent: "Unit 2",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "5",
+  //     Name: "Unit 5",
+  //     Parent: "Unit 3",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "6",
+  //     Name: "Unit 6",
+  //     Parent: "Unit 3",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "7",
+  //     Name: "Unit 7",
+  //     Parent: "Unit 3",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "8",
+  //     Name: "Unit 8",
+  //     Parent: "Unit 6",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "9",
+  //     Name: "Unit 9",
+  //     Parent: "Unit 6",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "10",
+  //     Name: "Unit 10",
+  //     Parent: "Unit 6",
+  //     Description: "This is unit 1"
+  //   }
+  // ]
+
+  
 }
