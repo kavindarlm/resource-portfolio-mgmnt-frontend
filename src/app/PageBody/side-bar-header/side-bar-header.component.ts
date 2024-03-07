68a2db61f13ac56a80bd524f58bf5fee614ce053
Janith Edirisinghe <janithchathusankaedirisinghe@gmail.com>
2024-03-08 01:24:16 +0530

    V6-sidebar

---
+++ src/app/PageBody/side-bar-header/side-bar-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar-header',
+  templateUrl: './side-bar-header.component.html',
+  styleUrl: './side-bar-header.component.css'
+})
+export class SideBarHeaderComponent {
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,27 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PagesBodyComponent } from './pages-body/pages-body.component';
+import { PagesBodyComponent } from './PageBody/pages-body/pages-body.component';
+import { FooterComponent } from './PageBody/footer/footer.component';
+import { PageHeaderComponent } from './PageBody/page-header/page-header.component';
+import { SideBarComponent } from './PageBody/side-bar/side-bar.component';
+import { SideBarHeaderComponent } from './PageBody/side-bar-header/side-bar-header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PagesBodyComponent,
+    FooterComponent,
+    PageHeaderComponent,
+    SideBarComponent,
+    SideBarHeaderComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
+  ], 
   providers: [
     provideClientHydration()
   ],
