39795d0f008be808093a3739f31bbee2c04a4992
Savindi <savindipaboda@gmail.com>
2024-03-09 08:36:05 +0530

    resourceCreation_using_json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesBodyComponent } from './pages-body/pages-body.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AddFormComponent } from './resourceMgt/add-form/add-form.component';
 import { ButtonComponent } from './resourceMgt/button/button.component';
 import { FirstViewComponent } from './resourceMgt/first-view/first-view.component';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'button', component: ButtonComponent},
+  { path: 'add-form', component: AddFormComponent}
+];
 
 
 @NgModule({
@@ -20,7 +28,10 @@ import { FirstViewComponent } from './resourceMgt/first-view/first-view.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
