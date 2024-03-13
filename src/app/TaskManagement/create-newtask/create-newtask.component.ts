ebc0916015c9f993f45a307c3f92b523363649d6
Janith Edirisinghe <janithchathusankaedirisinghe@gmail.com>
2024-03-13 23:10:52 +0530

    V1

--- src/app/TaskManagement/create-newtask/create-newtask.component.ts
+++ src/app/TaskManagement/create-newtask/create-newtask.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-newtask',
   templateUrl: './create-newtask.component.html',
   styleUrl: './create-newtask.component.css'
 })
-export class CreateNewtaskComponent {
-  TaskForm: FormGroup = new FormGroup({
-    taskName: new FormControl(''),
-  expectedStartDate: new FormControl(''),
-  expectedEndDate: new FormControl(''),
-  taskpersentage: new FormControl(''),
-  });
-}
+export class CreateNewtaskComponent implements OnInit {
+  taskForm!: FormGroup;
+  constructor(private formbulder: FormBuilder){}
+  ngOnInit(): void {
+      this.taskForm = this.formbulder.group({
+        taskName: ['',Validators.required],
+        expectedStartDate: ['',Validators.required],
+        expectedEndDate: ['',Validators.required],
+        taskpersentage: ['',Validators.required]
+      });
+
+      }
+  }
