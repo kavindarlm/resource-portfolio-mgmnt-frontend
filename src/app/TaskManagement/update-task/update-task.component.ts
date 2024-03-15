b83422d00ea11521c21ed60833817b75290b8d19
Janith Edirisinghe <janithchathusankaedirisinghe@gmail.com>
2024-03-15 18:46:31 +0530

    V4-ProjectManagement

--- src/app/TaskManagement/update-task/update-task.component.ts
+++ src/app/TaskManagement/update-task/update-task.component.ts
@@ -20,8 +20,15 @@ export class UpdateTaskComponent {
   //   this.sliderValue = (event.target as HTMLInputElement).valueAsNumber;
   // }
   sliderValue: number = 1;
+  trackGradient: string= ''; // CSS variable to hold the track gradient
 
   updateProgress(event: any) {
     this.sliderValue = event.target.value;
+
+    const percentage = (this.sliderValue - 1) / (100 - 1) * 100;
+    // Dynamically update the gradient based on the percentage
+    this.trackGradient = `linear-gradient(to right, #A9BCFF 0%, #A9BCFF ${percentage}%, #7752FE ${percentage}%, #7752FE 100%)`;
   }
+
+  
 }
