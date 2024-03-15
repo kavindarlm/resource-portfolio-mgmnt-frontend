66bc0847a147744561f1070ce2d7d0b3f68870f2
shiharadissanayake <99583495+shiharadissanayake@users.noreply.github.com>
2024-03-15 13:45:38 +0530

    sprint mgt into body

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { LoginAccComponent } from './login-acc/login-acc.component';
 import { FgPsswdComponent } from './fg-psswd/fg-psswd.component';
 import { PagesBodyComponent } from './PageBody/pages-body/pages-body.component';
 import { AdminDasbdBodyComponent } from './admin-dashboard/admin-dasbd-body/admin-dasbd-body.component';
+import { MainComponent } from './Sprint_Management/main/main.component';
+// import { SprintMgtComponent } from './Sprint_Management/sprint-mgt/sprint-mgt.component';
 
 const routes: Routes = [
   //redirect to login page
@@ -11,7 +13,13 @@ const routes: Routes = [
   { path: 'login', component: LoginAccComponent },
   { path: 'forgot-password', component: FgPsswdComponent },
   { path: 'pages-body', component: PagesBodyComponent },
-  { path: 'admin-dashboard', component: AdminDasbdBodyComponent}
+  { path: 'admin-dashboard', component: AdminDasbdBodyComponent},
+
+  { path: 'sprint-management', component: MainComponent},
+  // { path: 'sprint-mgt', component:SprintMgtComponent}
+
+  
+
   
   //wildcard route to handle 404
   //{ path: '**', component: PageNotFoundComponent } // PageNotFoundComponent needs to be created
