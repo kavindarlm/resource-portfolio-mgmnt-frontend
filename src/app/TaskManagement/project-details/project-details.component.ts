808ef23249ee81cffeb44c9e427725da39f9b2d5
Janith Edirisinghe <janithchathusankaedirisinghe@gmail.com>
2024-03-15 13:35:54 +0530

    V4

--- src/app/TaskManagement/project-details/project-details.component.ts
+++ src/app/TaskManagement/project-details/project-details.component.ts
@@ -22,4 +22,62 @@ export class ProjectDetailsComponent {
       deliveryManager: 'John Doe',
     }
   ];
+  tasks: any = [
+    {
+      "taskId": 1,
+      "expectedTaskPercentage": 20,
+      "taskProgress": 50
+    },
+    {
+      "taskId": 2,
+      "expectedTaskPercentage": 50,
+      "taskProgress": 75
+    },
+    {
+      "taskId": 3,
+      "expectedTaskPercentage": 30,
+      "taskProgress": 10
+    },
+    {
+      "taskId": 4,
+      "expectedTaskPercentage": 70,
+      "taskProgress": 30
+    },
+    {
+      "taskId": 5,
+      "expectedTaskPercentage": 10,
+      "taskProgress": 90
+    },
+    {
+      "taskId": 6,
+      "expectedTaskPercentage": 40,
+      "taskProgress": 20
+    },
+    {
+      "taskId": 7,
+      "expectedTaskPercentage": 60,
+      "taskProgress": 70
+    },
+    {
+      "taskId": 8,
+      "expectedTaskPercentage": 80,
+      "taskProgress": 45
+    },
+    {
+      "taskId": 9,
+      "expectedTaskPercentage": 90,
+      "taskProgress": 55
+    },
+    {
+      "taskId": 10,
+      "expectedTaskPercentage": 25,
+      "taskProgress": 85
+    }
+  ]
+  
+  handleTaskClick(data: any){
+    console.log(data);
+    
+  }
+
 }
