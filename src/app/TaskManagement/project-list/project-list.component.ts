99a2fed9ae089aca7ee21b2169eaf020eee74b8c
Janith Edirisinghe <janithchathusankaedirisinghe@gmail.com>
2024-03-16 15:41:53 +0530

    V2

--- src/app/TaskManagement/project-list/project-list.component.ts
+++ src/app/TaskManagement/project-list/project-list.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './project-list.component.css'
 })
 export class ProjectListComponent {
-
+  data : any = [
+    {
+      id: 1,
+      name: 'Project 1',
+      description: 'Project 1 description'
+    },
+    {
+      id: 2,
+      name: 'Project 2',
+      description: 'Project 2 description'
+    },
+    {
+      id: 3,
+      name: 'Project 3',
+      description: 'Project 3 description'
+    }
+  ]
 }
