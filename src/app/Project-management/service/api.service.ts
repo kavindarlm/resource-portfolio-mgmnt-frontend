0e66b9c56196e5c122b5e67fb1cb04c328fbc94d
Janith Edirisinghe <janithchathusankaedirisinghe@gmail.com>
2024-03-20 17:42:06 +0530

    V1-After create Project- Api

--- src/app/Project-management/service/api.service.ts
+++ src/app/Project-management/service/api.service.ts
@@ -10,14 +10,15 @@ export class ApiService {
   constructor(private http:HttpClient) { }
   //AddProject
   addProject(data:datamodel){
-    return this.http.post<datamodel>("http://localhost:3000/posts",data);
+    // return this.http.post<datamodel>("http://localhost:3000/posts",data);
+    return this.http.post<datamodel>("http://localhost:5000/projects",data);
   }
   //GetProjectDetails
   getProjectList(){
     return this.http.get<datamodel[]>("http://localhost:3000/posts");
   }
   //FetchProjectDetails
-  fetchProject(id:string){
+  fetchProject(id:string){ 
     return this.http.get<datamodel>("http://localhost:3000/posts/"+id);
   }
   //UpdateProjectDetails
