f6fa16dfee1bf99fd0579242b95203f8da54f0ea
Savindi <savindipaboda@gmail.com>
2024-04-11 23:04:21 +0530

    unit-list-done

--- src/app/orgUnitMgt/unit-list/unit-list.component.ts
+++ src/app/orgUnitMgt/unit-list/unit-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrganizationalUnitModel } from '../unit-form/unit-form.model';
+import { OrgUnitMgtService } from '../../shared/orgUnitMgt_services/orgUnitMgt.service';
 
 @Component({
   selector: 'app-unit-list',
@@ -7,80 +9,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnitListComponent implements OnInit{
   showForm = false;
-  constructor() {
+  orgunits : OrganizationalUnitModel[] | undefined;
+  constructor(private orgUnitMgtService: OrgUnitMgtService) {
     this.showForm=false;
   }
 
   ngOnInit(): void {
-
+    this.loadOrgUnits();
   }
 
   showcomponent(){
     this.showForm = !this.showForm;
   }
 
-  unitList: any = [
-    {
-      ID: "1",
-      Name: "Unit 1",
-      Parent: "",
-      Description: "This is unit 1"
-    },
-    {
-      ID: "2",
-      Name: "Unit 2",
-      Parent: "Unit 1",
-      Description: "This is unit 1"
-    },
-    {
-      ID: "3",
-      Name: "Unit 3",
-      Parent: "Unit 1",
-      Description: "This is unit 1"
-    },
-    {
-      ID: "4",
-      Name: "Unit 4",
-      Parent: "Unit 2",
-      Description: "This is unit 1"
-    },
-    {
-      ID: "5",
-      Name: "Unit 5",
-      Parent: "Unit 3",
-      Description: "This is unit 1"
-    },
-    {
-      ID: "6",
-      Name: "Unit 6",
-      Parent: "Unit 3",
-      Description: "This is unit 1"
-    },
-    {
-      ID: "7",
-      Name: "Unit 7",
-      Parent: "Unit 3",
-      Description: "This is unit 1"
-    },
-    {
-      ID: "8",
-      Name: "Unit 8",
-      Parent: "Unit 6",
-      Description: "This is unit 1"
-    },
-    {
-      ID: "9",
-      Name: "Unit 9",
-      Parent: "Unit 6",
-      Description: "This is unit 1"
-    },
-    {
-      ID: "10",
-      Name: "Unit 10",
-      Parent: "Unit 6",
-      Description: "This is unit 1"
-    }
-  ]
+  loadOrgUnits(){
+    this.orgUnitMgtService.getOrgUnits().subscribe(res=>{
+      this.orgunits = res;
+    })
+  }
+
+  // unitList: any = [
+  //   {
+  //     ID: "1",
+  //     Name: "Unit 1",
+  //     Parent: "",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "2",
+  //     Name: "Unit 2",
+  //     Parent: "Unit 1",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "3",
+  //     Name: "Unit 3",
+  //     Parent: "Unit 1",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "4",
+  //     Name: "Unit 4",
+  //     Parent: "Unit 2",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "5",
+  //     Name: "Unit 5",
+  //     Parent: "Unit 3",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "6",
+  //     Name: "Unit 6",
+  //     Parent: "Unit 3",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "7",
+  //     Name: "Unit 7",
+  //     Parent: "Unit 3",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "8",
+  //     Name: "Unit 8",
+  //     Parent: "Unit 6",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "9",
+  //     Name: "Unit 9",
+  //     Parent: "Unit 6",
+  //     Description: "This is unit 1"
+  //   },
+  //   {
+  //     ID: "10",
+  //     Name: "Unit 10",
+  //     Parent: "Unit 6",
+  //     Description: "This is unit 1"
+  //   }
+  // ]
 
   
 }
